fa2d23c8615cef0053761260935ee1e88e4de489
ericalundstrom <erica.lundstrom99@hotmail.com>
2024-04-14 18:29:49 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+function RenderPortfolio(params) {
+    let wrapper = document.querySelector("#wrapper");
+
+    wrapper.innerHTML = `
+        <h1 id="title"> Erica´s Portfolio </h1>
+    `;
+}
+
+
+RenderPortfolio()
